463a31dc168c4d8af227166e913f02b06ae910cb
DarkJediDJ <xintut@mail.ru>
2021-10-29 15:43:53 +0300

    Deleted calculator.go and + moved function into reverse.go

--- calculator.go
+++
@@ -1,6 +0,0 @@
-package strrvrs
-
-//Calculator provides way to count number of characters in the string
-func Calculator(s string) int {
-	return len([]rune(s))
-}
--- reverse.go
+++ reverse.go
@@ -9,3 +9,8 @@ func Reverse(s string) string {
 	}
 	return string(rns)
 }
+
+//Calculator provides way to count number of characters in the string
+func Calculator(s string) int {
+	return len([]rune(s))
+}
