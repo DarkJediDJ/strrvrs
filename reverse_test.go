5e0294715fb8b828b5a34bf253c3cc86ffcf163b
DarkJediDJ <xintut@mail.ru>
2021-11-01 14:09:54 +0300

    Tests fix

--- reverse_test.go
+++ reverse_test.go
@@ -10,14 +10,14 @@ func TestReverse(t *testing.T) {
 		text string
 		want string
 	}{
-		{"English reverse", "Hello, World.", ".dlroW ,olleH"},
-		{"Russian reverse", "Привет, мир.", ".рим ,тевирП"},
-		{"Chinese reverse", "你好, 世界。", "。界世 ,好你"},
-		{"Emojis reverse", "\U0001f60a \U0001f914 \U0001f644 \U0001f925 \U0001f634 \U0001f635", "\U0001f635 \U0001f634 \U0001f925 \U0001f644 \U0001f914 \U0001f60a"},
-		{"Emoji reverse", "\U0001f60a", "\U0001f60a"},
-		{"Empty reverse", "", ""},
-		{"Tabs and spaces reverse", "  		", "		  "},
-		{"Long string reverse", "Ob0ZoEDYxoPEYLXtVXGCm6fldgpucVSJxjwP5ae3BWiRgVoH4B5s", "s5B4HoVgRiWB3ea5PwjxJSVcupgdlf6mCGXVtXLYEPoxYDEoZ0bO"},
+		{"English", "Hello, World.", ".dlroW ,olleH"},
+		{"Russian", "Привет, мир.", ".рим ,тевирП"},
+		{"Chinese", "你好, 世界。", "。界世 ,好你"},
+		{"Emojis", "\U0001f60a \U0001f914 \U0001f644 \U0001f925 \U0001f634 \U0001f635", "\U0001f635 \U0001f634 \U0001f925 \U0001f644 \U0001f914 \U0001f60a"},
+		{"Emoji", "\U0001f60a", "\U0001f60a"},
+		{"Empty", "", ""},
+		{"Tabs and spaces", "  		", "		  "},
+		{"Long string", "Ob0ZoEDYxoPEYLXtVXGCm6fldgpucVSJxjwP5ae3BWiRgVoH4B5s", "s5B4HoVgRiWB3ea5PwjxJSVcupgdlf6mCGXVtXLYEPoxYDEoZ0bO"},
 	}
 	for _, tc := range testReverseCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -34,14 +34,14 @@ func TestCalculator(t *testing.T) {
 		text string
 		want int
 	}{
-		{"English calculator", "Example", 7},
-		{"Russian calculator", "Пример строки", 13},
-		{"Chinese calculator", "你好, 世界。", 7},
-		{"Emojis calculator", "\U0001f60a \U0001f616 \U0001f644 \U0001f925 \U0001f634 \U0001f635 \U0001f480", 13},
-		{"Emoji calculator", "\U0001f60a", 1},
-		{"Empty calculator", "", 0},
-		{"Tabs and spaces calculator", "  		", 4},
-		{"Long string calculator", "oy4iGAPpxkz1NfJd4wak3wAIZ4xfSUFOjlTpLoO1OEQd26vXaBCS23cSnCPPFSOBI0H44T", 70},
+		{"English", "Example", 7},
+		{"Russian", "Пример строки", 13},
+		{"Chinese", "你好, 世界。", 7},
+		{"Emojis", "\U0001f60a \U0001f616 \U0001f644 \U0001f925 \U0001f634 \U0001f635 \U0001f480", 13},
+		{"Emoji", "\U0001f60a", 1},
+		{"Empty", "", 0},
+		{"Tabs and spaces", "  		", 4},
+		{"Long string", "oy4iGAPpxkz1NfJd4wak3wAIZ4xfSUFOjlTpLoO1OEQd26vXaBCS23cSnCPPFSOBI0H44T", 70},
 	}
 	for _, tc := range testCalculatorCases {
 		t.Run(tc.name, func(t *testing.T) {
