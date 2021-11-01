eca67d62c24026a737be0b905fd15b442406036c
DarkJediDJ <xintut@mail.ru>
2021-11-01 09:59:31 +0300

    Test fixes

--- reverse_test.go
+++ reverse_test.go
@@ -1,7 +1,6 @@
 package strrvrs
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -11,17 +10,17 @@ func TestReverse(t *testing.T) {
 		text string
 		want string
 	}{
-		{"Simple string", "Hello, World.", ".dlroW ,olleH"},
-		{"Russian string", "Привет, мир.", ".рим ,тевирП"},
-		{"Chinese string", "你好, 世界。", "。界世 ,好你"},
-		{"Lots of emojis", "\U0001f60a \U0001f914 \U0001f644 \U0001f925 \U0001f634 \U0001f635", "\U0001f635 \U0001f634 \U0001f925 \U0001f644 \U0001f914 \U0001f60a"},
-		{"One emoji", "\U0001f60a", "\U0001f60a"},
-		{"Empty string", "", ""},
-		{"String with tabs and spaces", "  		", "		  "},
-		{"Very long string", "Ob0ZoEDYxoPEYLXtVXGCm6fldgpucVSJxjwP5ae3BWiRgVoH4B5s", "s5B4HoVgRiWB3ea5PwjxJSVcupgdlf6mCGXVtXLYEPoxYDEoZ0bO"},
+		{"English reverse", "Hello, World.", ".dlroW ,olleH"},
+		{"Russian reverse", "Привет, мир.", ".рим ,тевирП"},
+		{"Chinese reverse", "你好, 世界。", "。界世 ,好你"},
+		{"Emojis reverse", "\U0001f60a \U0001f914 \U0001f644 \U0001f925 \U0001f634 \U0001f635", "\U0001f635 \U0001f634 \U0001f925 \U0001f644 \U0001f914 \U0001f60a"},
+		{"Emoji reverse", "\U0001f60a", "\U0001f60a"},
+		{"Empty reverse", "", ""},
+		{"Tabs and spaces reverse", "  		", "		  "},
+		{"Long string reverse", "Ob0ZoEDYxoPEYLXtVXGCm6fldgpucVSJxjwP5ae3BWiRgVoH4B5s", "s5B4HoVgRiWB3ea5PwjxJSVcupgdlf6mCGXVtXLYEPoxYDEoZ0bO"},
 	}
 	for _, tc := range testReverseCases {
-		t.Run(fmt.Sprintf("Couldnt reverse string: %s in case : %s", tc.text, tc.name), func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			if got := Reverse(tc.text); got != tc.want {
 				t.Errorf("got %s; want %s", got, tc.want)
 			}
@@ -35,17 +34,17 @@ func TestCalculator(t *testing.T) {
 		text string
 		want int
 	}{
-		{"Simple string", "Example", 7},
-		{"Russian string", "Россия!!!", 9},
-		{"Chinese string", "你好, 世界。", 7},
-		{"Lots of emojis", "\U0001f60a \U0001f616 \U0001f644 \U0001f925 \U0001f634 \U0001f635 \U0001f480", 13},
-		{"One emoji", "\U0001f60a", 1},
-		{"Empty string", "", 0},
-		{"String with tabs and spaces", "  		", 4},
-		{"Very long string", "oy4iGAPpxkz1NfJd4wak3wAIZ4xfSUFOjlTpLoO1OEQd26vXaBCS23cSnCPPFSOBI0H44T", 70},
+		{"English calculator", "Example", 7},
+		{"Russian calculator", "Пример строки", 13},
+		{"Chinese calculator", "你好, 世界。", 7},
+		{"Emojis calculator", "\U0001f60a \U0001f616 \U0001f644 \U0001f925 \U0001f634 \U0001f635 \U0001f480", 13},
+		{"Emoji calculator", "\U0001f60a", 1},
+		{"Empty calculator", "", 0},
+		{"Tabs and spaces calculator", "  		", 4},
+		{"Long string calculator", "oy4iGAPpxkz1NfJd4wak3wAIZ4xfSUFOjlTpLoO1OEQd26vXaBCS23cSnCPPFSOBI0H44T", 70},
 	}
 	for _, tc := range testCalculatorCases {
-		t.Run(fmt.Sprintf("Couldnt count chars in string: %s in case : %s", tc.text, tc.name), func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			if got := Calculator(tc.text); got != tc.want {
 				t.Errorf("got %d\n; want %d\n", got, tc.want)
 			}
